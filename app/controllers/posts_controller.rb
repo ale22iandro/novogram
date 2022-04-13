2129147f1fb2c6f699950ea038c0655bbe0e099a
ale22iandro <ale22iandro@gmail.com>
2022-04-13 08:30:57 -0700

    edit post description added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def like
-    @post=Post.all.find(params[:id])
+    @post = Post.all.find(params[:id])
     if @post.liked?(current_user)
       @post.likes.find_by(user_id: current_user.id).destroy
     else
@@ -14,20 +14,39 @@ class PostsController < ApplicationController
 
     redirect_to post_path(@post)
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+           redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def show
     #@new_comment = @post.comments.build(params[:comment])
     @post = Post.find(params[:id])
   end
+
   def create
     Post.create(post_params)
     redirect_to root_path
   end
+
   def destroy
     @post = current_user.posts.find(params[:id])
     @post.destroy
     redirect_to user_path(current_user)
   end
+
   private
+
   def post_params
     params.require(:post).permit(:description, :image, :user_id)
   end
